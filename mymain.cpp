ae67cbd1296e74298228aa9d455557208510cb42
SuvorovJA <riskiroy@gmail.com>
2018-03-14 16:13:18 +0700

    мелкие правки демо

--- mymain.cpp
+++ mymain.cpp
@@ -9,20 +9,16 @@ int mymain(int argc, char **argv)
     // печать переданных аргументов
     cout << "принято " << argc << " аргументов" << endl;
     if (argc > 0) for (int i = 0; i < argc; ++i) cout << i + 1 << ":" << argv[i] << endl;
-    cout << "---------" << endl;
 
-    // вывод значений
+    cout << "--------- демонстрация вывода ---------" << endl;
     for (int i = 0; i < 3; i++) {
         cout << i << " вывод@cout" << "\t" << "/forloop/rus/tab" << endl;
         cerr << hex << (i + 1) * 1111 << "@cerr" << "/forloop/hex" << endl;
         clog << setw(6) << setprecision(3) <<  float((i + 0.1) * 10 / 3.1) << "@clog" << "/forloop/fload" << endl;
     }
-    cout << "---------" << endl;
 
-
-    // ввод значений
+    cout << "--------- демонстрация ввода значений через dialog ---------" << endl;
     int i = 9;
-//    cin >> i; // не работает пока
     DialogBoxSS *dbox = MainWindow::dbholder; // лишний мэджик :( TODO вынести за пределы main
     cout << dbox->getString("Введите что-нибудь...") << endl;
     i = stoi(dbox->getString("Введите число:")); // c конвертацией в int через stoi
@@ -34,5 +30,8 @@ int mymain(int argc, char **argv)
     i = 0;
     ss >> dv >> i ;
     cout << "Вы ввели \"" << setw(6) << setprecision(3) << dv << "\" и \"" << i << "\"" << endl;
+
+
+    //  cin >> i; // не работает пока
     return i;
 }
